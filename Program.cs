7cbae01616c47f135458e713d039b2eaf63778ce
AlpDereli <alpdereli26@gmail.com>
2023-08-25 20:21:55 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter a word: ");
+string s = Console.ReadLine();
+string[] arr = s.Split(" ");
+foreach (string str in arr)
+{
+    char s1 = str[0];
+    char s2 = str[str.Count()-1];
+    string ilk = str.Remove(0, 1);
+    string iki = ilk.Remove(ilk.Count()-1, 1);
+    string fin = s2 + iki + s1;
+    Console.Write(fin + " ");
+}
